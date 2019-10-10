5d67b40f7f06877a660bd6bc4572acdbd4f60769
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2019-10-10 19:43:12 +0200

    Start to implement the soft reset mode

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -43,20 +43,68 @@ stage_2:
 		jmp $
 	.it_seems_a_valid_drive_number:
 
-	; Checking that the whole stage was loaded.
+	call .check_magic
+
+	; The MBR should be at 0x7C00 already.
+	call .copy_mbr_payload
+
+	xor BX, BX
+	mov AH, 0x0E
+	mov AL, 't'
+	int 0x10
+
+	; TODO: Remove when done.
+	jmp .softreset
+
+	jmp $
+
+.softreset:
+	; Stack setup for stage 2.
+	mov BP, .stack_segment
+	mov SS, BP
+	mov SP, 0xFFFF
+
+	; Data segment initialization.
+	mov AX, CS
+	mov DS, AX
+
+	cld
+	xor BX, BX
+
+	call enforce_tty_video_mode
+	call .check_magic
+	; TODO: reload the MBR.
+	;call .copy_mbr_payload
+
+	; TODO: Remove when done.
+	xor BX, BX
+	mov AH, 0x0E
+	mov AL, 'X'
+	int 0x10
+
+	jmp $
+
+.boot_drive db 0x00 ; It must be loaded at runtime.
+
+.boot_drive_error db "!BOOTDRIVE:", 0
+.incomplete_error db "!INCOMPLETE", 0
+
+.check_magic:
+	; Checking that the whole stage was loaded/overwritten.
 	cmp word [s2_magic_number], 0xABD5
 	je .ready
 	.incomplete_load:
-		; xor BX, BX
 		mov AH, 0x0E
 		mov SI, .incomplete_error
 		call print_string
 		jmp $
 	.ready:
+	ret
 
-	; Copying the payload from stage 1.
-	; (The MBR would have to be reloaded in soft reset mode.)
-	; Possible optimization: use movsd instead.
+.copy_mbr_payload:
+	; Copying the MBR payload from its copy at 0x7C00.
+	; The MBR has to be reloaded in soft reset mode,
+	; but it is already there when coming from stage 1.
 	mov CX, 0x07C0
 	mov DS, CX
 	mov SI, 0x200 - 72
@@ -65,24 +113,11 @@ stage_2:
 	mov ES, CX
 	mov DI, s2_mbr_payload
 
-	mov CX, 70 / 2
-	rep movsw
+	mov CX, (70 - 2) / 4
+	rep movsd
+	movsw ; Copies the last two bytes.
 
-	xor BX, BX
-	mov AH, 0x0E
-	mov AL, 't'
-	int 0x10
-
-	jmp $
-
-.softreset:
-	jmp $
-
-.boot_drive db 0x00 ; It must be loaded at runtime.
-
-.boot_drive_error db "!BOOTDRIVE:", 0
-.incomplete_error db "!INCOMPLETE", 0
-
+%include 'boot/video_16.asm'
 %include 'boot/print_16/hex.asm'
 %include 'boot/print_16/string.asm'
 
