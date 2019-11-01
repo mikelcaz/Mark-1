10a85c3f3476805c0ba0a7ac80da981aead772ca
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2019-11-01 18:52:14 +0100

    Enable the 32-bit basic flat mode (print something)

--- kernel/entry.asm
+++ kernel/entry.asm
@@ -1,6 +1,81 @@
 [bits 16]
 
-mov AX, 0x0E00 + 'K'
-xor BX, BX
-int 0x10
-jmp $
+s2_sectors EQU 3
+correction EQU 0x0500 + s2_sectors * 0x200
+[org correction]
+
+jmp entry_16
+
+entry_32_addr:
+	; Order matters.
+	.offset dw entry_32 ; Be careful with the offset!
+	.segment dw gdt.code_sel
+
+%include 'kernel/gdt.asm'
+
+entry_16:
+	mov AX, 0x0E00 + 'K'
+	xor BX, BX
+	int 0x10
+
+	mov AX, CS
+	mov DS, AX
+
+	cli
+	lgdt [gdt_descriptor - correction]
+
+	mov EAX, CR0
+	or EAX, 0x1
+	mov CR0, EAX
+
+	jmp far [entry_32_addr - correction]
+
+
+[bits 32]
+
+entry_32:
+	.video_mem EQU 0xB8000
+	.white_on_black EQU 0x07
+	.blue_on_white EQU 0xF9
+.begin:
+	mov AX, gdt.data_sel
+	mov DS, AX
+	mov ES, AX
+	mov FS, AX
+	mov GS, AX
+	mov SS, AX
+
+	; TODO: Limit the stack growing in the GDT.
+	mov EBP, 0x70000
+	mov ESP, EBP
+
+	mov EDX, .video_mem + 2 * 80 * 24
+
+	mov AH, .blue_on_white
+	mov SI, .msg_32_bit_mode
+	call print_string
+
+	jmp $
+
+.msg_32_bit_mode db '32-bit mode', 0
+
+print_string:
+; EDX = memory address
+; SI = message
+; AH = format
+; Mod: EDX
+	push SI
+	push AX
+
+	jmp .init
+	.loop:
+		mov [EDX], AX
+		add EDX, 2
+	.init:
+		lodsb
+		cmp AL, 0
+	jne .loop
+
+	pop AX
+	pop SI
+	ret
---
+++ kernel/gdt.asm
@@ -0,0 +1,44 @@
+; G(ranularity, *4KiB)
+; D/B (default operation size, 0 means 16 bits, 1 means 32 bits.)
+; L(ong mode, i.e. 64-bit code descriptor, reserved for data segments.)
+; AVL (whatever the OS wants.)
+
+; P(resent)
+; DPL[2] (Descriptor Privilege Level)
+; S (0 means "system", 1 means "code or data".)
+; TYPE bits:
+;	#3: Code
+;	#2: Conforming
+;	#1: Readable (0 means "execute-only".)
+;	#0: Accessed (set by the CPU when the segment is accessed.)
+
+gdt:
+	.code_sel EQU gdt.code - gdt.begin
+	.data_sel EQU gdt.data - gdt.begin
+
+.begin:
+	; The first segment descriptor must be a null descriptor.
+	dq 0x0000000000000000
+
+.code:
+	dw 0xFFFF ; Limit least significant bits (16/20)
+	dw 0x0000 ; Base least significant bits (16/32)
+	db 0x00 ; Base middle bits (8/32)
+	db 10011010b ; P, DPL, S, Type
+	db 11001111b ; G, D/B, L, AVL, Limit most significant bits (4/20)
+	db 0x00 ; Base most significant bits (8/32)
+
+.data:
+	dw 0xFFFF ; Limit least significant bits (16/20)
+	dw 0x0000 ; Base least significant bits (16/32)
+	db 0x00 ; Base middle bits (8/32)
+	db 10010010b ; P, DPL, S, Type
+	db 11001111b ; G, D/B, L, AVL, Limit most significant bits (4/20)
+	db 0x00 ; Base most significant bits (8/32)
+
+.end:
+
+; Must be loaded to the GDTR.
+gdt_descriptor:
+	.size dw gdt.end - gdt.begin - 1
+	.start dd gdt.begin ; Be careful with the offset!
