d516ea6e666250746b8dfbac06458b91e8ccae20
Mikel Cazorla Pérez <mikelcaz.dev@gmail.com>
2019-11-09 12:45:45 +0100

    Add essential functions to work with strings

--- libcmin/include/string.h
+++ libcmin/include/string.h
@@ -2,3 +2,10 @@ int memcmp(void const *a, void const *b, size_t n);
 void *memcpy(void *dst, void const *src, size_t n);
 void *memmove(void *dst, void const *src, size_t n);
 void *memset(void *mem, int val, size_t n);
+
+size_t strlen(char const *str);
+size_t strnlen(char const *str, size_t maxlen);
+int strcmp(char const *a, char const *b);
+int strncmp(char const *a, char const *b, size_t upto);
+char *strcpy(char *dst, char const *src);
+char *strncpy(char *dst, char const *src, size_t n);
--- libcmin/string.c
+++ libcmin/string.c
@@ -57,3 +57,80 @@ memset(void *mem, int val, size_t n)
 
 	return mem;
 }
+
+size_t
+strlen(char const *str)
+{
+	return strnlen(str, -1);
+}
+
+size_t
+strnlen(char const *str, size_t maxlen)
+{
+	size_t n;
+
+	for (n = 0; n < maxlen; ++n)
+		if (str[n] == '\0')
+			break;
+
+	return n;
+}
+
+int
+strcmp(char const *a, char const *b)
+{
+	while (*a != '\0' && *a == *b) {
+		++a;
+		++b;
+	}
+
+	if (*b < *a)
+		return 1;
+	if (*a < *b)
+		return -1;
+
+	return 0;
+}
+
+int
+strncmp(char const *a, char const *b, size_t upto)
+{
+	for (size_t i = 0; i < upto; ++i) {
+		if (a[i] != '\0' && a[i] == b[i])
+			continue;
+
+		if (b[i] < a[i])
+			return 1;
+		if (a[i] < b[i])
+			return -1;
+
+		break;
+	}
+
+	return 0;
+}
+
+char *
+strcpy(char *dst, char const *src)
+{
+	char * const fw = dst;
+	while ((*dst++ = *src++));
+	return fw;
+}
+
+char *
+strncpy(char *dst, char const *src, size_t n)
+{
+	char * const fw = dst;
+
+	do {
+		if (n == 0)
+			break;
+		--n;
+	} while ((*dst++ = *src++));
+
+	for (; 0 < n; --n)
+		dst[n - 1] = 0;
+
+	return fw;
+}
